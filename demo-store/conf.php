066005592fb287f692dd2b51097f269f42b93319
Alfred Robert Rowe <alfred@ncodedevlabs.com>
2012-10-28 16:11:49 +0000

    Configuration updates

--- demo-store/conf.php
+++ demo-store/conf.php
@@ -1,9 +1,9 @@
 <?php
-MPower_Setup::setMasterKey(YOUR_API_MASTER_KEY);
-MPower_Setup::setPublicKey(YOUR_API_PUBLIC_KEY);
-MPower_Setup::setPrivateKey(YOUR_API_PRIVATE_KEY);
-MPower_Setup::setMode(["test"|"live"]);
-MPower_Setup::setToken(YOUR_API_TOKEN);
+MPower_Setup::setMasterKey("YOUR_API_MASTER_KEY");
+MPower_Setup::setPublicKey("YOUR_API_PUBLIC_KEY");
+MPower_Setup::setPrivateKey("YOUR_API_PRIVATE_KEY");
+MPower_Setup::setMode("test");
+MPower_Setup::setToken("YOUR_API_TOKEN");
 
 // Configure Checkout Store
 MPower_Checkout_Store::setName("Awesome Online Store");
@@ -12,5 +12,5 @@ MPower_Checkout_Store::setPhoneNumber("0244000001");
 MPower_Checkout_Store::setPostalAddress("P. O. Box AN 1010. Accra - Ghana.");
 
 // You may set this to your localhost for local testing of redirects
-// MPower_Checkout_Store::setCancelUrl("http://www.smsgh.com/");
-// MPower_Checkout_Store::setReturnUrl("http://www.ncodedevlabs.com/");
+// MPower_Checkout_Store::setCancelUrl("http://".$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME'])."/index.php");
+// MPower_Checkout_Store::setReturnUrl("http://".$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME'])."/confirm.php");
